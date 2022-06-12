b8ee9bda7adbbce146e349f976215671193d9d1f
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-12 13:55:01 +0300

    add react context api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+import AuthContext from './store/auth-context';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -26,16 +28,20 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
       <MainHeader
-        isAuthenticated={isLoggedIn}
+        /* isAuthenticated={isLoggedIn} */
         onLogout={logoutHandler}
       />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
